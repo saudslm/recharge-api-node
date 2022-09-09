13222a8d95e9e0c97f7bfcfbaa70d0c3f0424a0d
Salvador Espinosa Tabaco <salvador_espinosa@condenast.com>
2022-09-09 14:16:23 -0500

    Add endpoint to get customer portal url

--- resources/customer.js
+++ resources/customer.js
@@ -4,7 +4,7 @@ const assign = require('lodash/assign');
 
 const base = require('../mixins/base');
 
-function Customer(recharge){
+function Customer(recharge) {
     this.recharge = recharge;
 
     this.name = 'customers';
@@ -16,6 +16,11 @@ assign(Customer.prototype, base);
 Customer.prototype.getPaymentSources = function paymentSources(id) {
     const url = this.buildUrl(id, undefined, 'payment_sources');
     return this.recharge.request(url, 'GET', 'payment_sources');
+};
+
+Customer.prototype.getPortalUrl = function portalUrl(id) {
+    const url = this.buildUrl(id, undefined, 'portal_url');
+    return this.recharge.request(url, 'GET', 'portal_url');
 };
 
 module.exports = Customer;
