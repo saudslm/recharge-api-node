a21a5f64d2c2c798e76a6982fc2daa5368b15be6
Vishu Kumar <112818622+Vishu1007@users.noreply.github.com>
2024-03-06 15:25:40 +0530

    Update charge.js

--- resources/charge.js
+++ resources/charge.js
@@ -100,4 +100,15 @@ Charge.prototype.removeDiscount = function removeDiscount(charge_id, params){
   return this.recharge.request(url, 'POST', this.key, params);
 }
 
+/**
+ * Process the charge by using chargeID.
+ *
+ * @param {Number} id Charge ID
+ * @return {Promise} Promise that resolves with the result
+ * @public
+ */
+Charge.prototype.process = function processCharge(charge_id){
+  const url = this.buildUrl(charge_id, undefined, 'process');
+  return this.recharge.request(url, 'POST', this.key, charge_id);
+}
 module.exports = Charge;
